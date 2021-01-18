0f1cb8babe72b60f02878ba99ea91f699d15b886
hiroya <hiroya19890721@gmail.com>
2021-01-18 23:07:46 +0900

    Board.vue作成

---
+++ src/components/Board.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <header>
+      MY MEMO LIST
+    </header>
+    <main>
+      <p class="info-line">ALL: 0 task</p>
+    </main>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
---
+++ src/components/ListAdd.vue
@@ -0,0 +1,16 @@
+<template>
+  <form class="addlist" @submit.prevent="addList">
+    <input v-model = "title"
+    type="text"
+    class="text-input"
+    placeholder="Add new list">
+    <button> type</button>
+
+  </form>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
